c62f86bf5365f714ae74f0a8a09a5d421390c840
samzzii <nasstromsamuel@icloud.com>
2023-12-09 15:53:27 +0100

    added component for the looping as well

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import CategoryItem from './components/category-item/category-item.component';
-
+import CategoryMenu from "./components/category-menu/category-menu.component";
 
 const App = () => {
-  
   const categories = [
     {
-      "id": 1,
-      "title": "One Piece",
-      "imageUrl": "https://i.imgur.com/1gjkQ2t.jpg"
+      id: 1,
+      title: "One Piece",
+      imageUrl: "https://i.imgur.com/1gjkQ2t.jpg",
     },
     {
-      "id": 2,
-      "title": "Naruto",
-      "imageUrl": "https://i.imgur.com/qweq6kH.jpg"
+      id: 2,
+      title: "Naruto",
+      imageUrl: "https://i.imgur.com/qweq6kH.jpg",
     },
     {
-      "id": 3,
-      "title": "Dragon Ball",
-      "imageUrl": "https://i.imgur.com/WrbFJNx.jpg"
+      id: 3,
+      title: "Dragon Ball",
+      imageUrl: "https://i.imgur.com/WrbFJNx.jpg",
     },
     {
-      "id": 4,
-      "title": "Demon Slayer",
-      "imageUrl": "https://i.imgur.com/5kGSoAH.jpg"
+      id: 4,
+      title: "Demon Slayer",
+      imageUrl: "https://i.imgur.com/5kGSoAH.jpg",
     },
     {
-      "id": 5,
-      "title": "Pokémon",
-      "imageUrl": "https://i.imgur.com/lxYYEmi.jpg"
-    }
+      id: 5,
+      title: "Pokémon",
+      imageUrl: "https://i.imgur.com/lxYYEmi.jpg",
+    },
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
-};
+    <CategoryMenu categories={categories} />
+  )
+}
 
 export default App;
