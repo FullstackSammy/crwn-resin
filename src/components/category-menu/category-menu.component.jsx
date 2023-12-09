f9050754d30bc0f1ba85aa12caef9771e9b3bcac
samzzii <nasstromsamuel@icloud.com>
2023-12-09 15:59:04 +0100

    changed name of the styles file and importet it to component

--- src/components/category-menu/category-menu.component.jsx
+++ src/components/category-menu/category-menu.component.jsx
@@ -1,6 +1,6 @@
 import CategoryItem from "../category-item/category-item.component";
 
-import "./categories.styles.scss";
+import "./category-menu.styles.scss";
 
 const CategoryMenu = ({ categories }) => {
   
