2d4f2459dc2af8ac8c2190b250434f3589be6c4e
samzzii <nasstromsamuel@icloud.com>
2023-12-09 12:48:55 +0100

    added basic structure for categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 const App = () => {
   return (
-    <div>
-    <h1>Hello world!</h1>
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-cointainer">
+          <h2>One Piece</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-cointainer">
+          <h2>Naruto</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-cointainer">
+          <h2>Dragon Ball</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-cointainer">
+          <h2>Demon Slayer</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-cointainer">
+          <h2>Pokémon</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
