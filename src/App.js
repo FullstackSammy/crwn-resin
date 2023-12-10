d10f20cfa051d69a09b172599f15debd9b4c07ee
samzzii <nasstromsamuel@icloud.com>
2023-12-10 18:44:18 +0100

    added border-box to all elements and some styling to the navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 // Denna return fungerar precis som en Django url. du ger den en path och sedan vilket element den ska rendera
 
+const Shop = () => {
+  return <h1>HEY! I AM THE SHOP PAGE</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />{" "}
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
         {/* Det är så här man får navigationen att alltid synas på sidan. Home grejen är också bra för key-wordet index gör att det kommer synas tillsammans med navbaren. */}
       </Route>
     </Routes>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Fragment } from "react"; // Fragment är en component som renderar ingenting.
+import { Outlet, Link } from "react-router-dom"; // Link är precis som a-tag men används i samband med React Router.
+
+import "./navigation.styles.scss";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"; // Importerar logo från assets-mappen och gör den till en component som du kan använda i din navbar.
 
 const Navigation = () => {
-    return (
-      <div>
-        <div>
-          <h1>Hey!</h1>
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
         </div>
-        <Outlet />
       </div>
-      
-    );
-  };
+      <Outlet />
+    </Fragment>
+  );
+};
 
-  export default Navigation;
+export default Navigation;
