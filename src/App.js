62b464bbf11800badb855629bc07dda2a51d69e6
samzzii <nasstromsamuel@icloud.com>
2023-12-10 18:19:33 +0100

    added basic structure and file structure for navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Routes, Route } from "react-router-dom"; // Måste importas för att routing ska fungera
+
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+// Denna return fungerar precis som en Django url. du ger den en path och sedan vilket element den ska rendera
 
 const App = () => {
- 
-
   return (
-    <Home />
-  )
-}
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />{" "}
+        {/* Det är så här man får navigationen att alltid synas på sidan. Home grejen är också bra för key-wordet index gör att det kommer synas tillsammans med navbaren. */}
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
