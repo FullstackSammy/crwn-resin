159f941994ac3f8e6ccd66ec4d48708fb43e3cc5
samzzii <nasstromsamuel@icloud.com>
2023-12-11 18:03:43 +0100

    added signin with google

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,17 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAi0oKv42n7isYzscBKtK2SvbyxpGa3opw", // Not important that it is shown. Read the firebase documentation for more info.
-    authDomain: "crwn-resin-db.firebaseapp.com",
-    projectId: "crwn-resin-db",
-    storageBucket: "crwn-resin-db.appspot.com",
-    messagingSenderId: "1014700222801",
-    appId: "1:1014700222801:web:b849788ac88f0db68d0b70"
+  apiKey: "AIzaSyAi0oKv42n7isYzscBKtK2SvbyxpGa3opw", // Not important that it is shown. Read the firebase documentation for more info.
+  authDomain: "crwn-resin-db.firebaseapp.com",
+  projectId: "crwn-resin-db",
+  storageBucket: "crwn-resin-db.appspot.com",
+  messagingSenderId: "1014700222801",
+  appId: "1:1014700222801:web:b849788ac88f0db68d0b70",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account" // Varje gång någon interactar med våran providet så måste de alltid välja ett account.
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
