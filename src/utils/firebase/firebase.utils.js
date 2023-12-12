6664436cd31ee4bff684602f06656a7e47cfa9eb
samzzii <nasstromsamuel@icloud.com>
2023-12-12 21:34:52 +0100

    startedt adding database

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,12 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import {
+  getFirestore,
+  doc, // Needed to get a document instance
+  getDoc, // getting the data on that doc instance
+  setDoc, // setting the data on that doc instance
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,8 +29,20 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: "select_account" // Varje gång någon interactar med våran providet så måste de alltid välja ett account.
+  prompt: "select_account", // Varje gång någon interactar med våran providet så måste de alltid välja ett account.
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(); // Made a database instance
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // doc tar tre arguments. database, collection name, och en identifier
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+};
